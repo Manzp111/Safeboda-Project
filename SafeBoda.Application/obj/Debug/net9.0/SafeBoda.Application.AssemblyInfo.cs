0c516a1a131a804c2dcf513b2c33a35c40496473
Manzp111 <gilbertnshimyimana11@gmail.com>
2025-10-29 11:25:49 +0200

    rebase 2

--- SafeBoda.Application/obj/Debug/net9.0/SafeBoda.Application.AssemblyInfo.cs
+++ SafeBoda.Application/obj/Debug/net9.0/SafeBoda.Application.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeBoda.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f774f64233ca0532b378f75645a370a797f7430")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f59fa2c4c665455d35e333a953688d9a69aaa98")]
+>>>>>>> bffb53d (creating Safeboda.application for business logic)
 [assembly: System.Reflection.AssemblyProductAttribute("SafeBoda.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeBoda.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
